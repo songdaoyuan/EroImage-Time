f1b7cd80b792334d46f58d99f86332f045beed6a
宋道源 <32031697+songdaoyuan@users.noreply.github.com>
2019-04-16 22:17:18 +0800

    Update spidercore.py

--- spidercore.py
+++ spidercore.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
+
+#pip install -U requests[socks]
 import os
 import re
 
 import requests
 from bs4 import BeautifulSoup
 
+proxyon = False
+
 PixivMaleLike = 'https://www.pixiv.net/ranking.php?mode=male'
 PixivMakleLike_R18 = 'https://www.pixiv.net/ranking.php?mode=male_r18'
 PixivMaleLikeLogin = r'https://accounts.pixiv.net/login?return_to=http%3A%2F%2Fwww.pixiv.net%2Franking.php%3Fmode%3Dmale&source=pc&view_type=page'
 PixivLogin_api = 'https://accounts.pixiv.net/api/login?lang=zh'
+
+def enableproxy(bool):
+    proxie = {
+        'http': 'socks5://127.0.0.1:1080',
+        'https': 'socks5://127.0.0.1:1080'
+    }
+    if bool:
+        return proxie
+    else:
+        proxie = {}
+        return proxie
 
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0',
@@ -16,7 +31,7 @@ header = {
 }
 
 psession = requests.session()  # psession.cookies
-r = psession.get(PixivMaleLikeLogin, headers=header)
+r = psession.get(PixivMaleLikeLogin, headers=header, proxies = enableproxy(proxyon))
 content = r.content.decode('utf-8')
 postkey = re.findall(r"<input.+?name=\"post_key\".+?>",
                      content)[0].split(" ")[3].split("\"")[1]
@@ -28,7 +43,7 @@ pdata = {
 }
 r = psession.post(PixivLogin_api, headers=header, data=pdata)
 if  r.status_code == 200:
-    r = psession.get(PixivMakleLike_R18, headers=header)
+    r = psession.get(PixivMakleLike_R18, headers=header, proxies = enableproxy(proxyon))
     html = r.content.decode('utf-8') 
     soup = BeautifulSoup(html, 'lxml') #一页提供了40张色图 
     '''
